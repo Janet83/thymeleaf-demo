ee17d0d8870cb61d32ffab3604bbe68d077b61fb
Janet <zan.smigiel@gmail.com>
2022-06-12 11:55:59 +0200

    thymeleaf-demo initializing

---
+++ src/main/java/org/wsb/thymeleaddemo/ThymeleadDemoApplication.java
@@ -0,0 +1,13 @@
+package org.wsb.thymeleaddemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeleadDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThymeleadDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/wsb/thymeleaddemo/ThymeleadDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.wsb.thymeleaddemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeleadDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
